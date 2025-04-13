21116705e54b3a4fca074236b27eb23d358ffc93
joaopedroldavid-del <joaopedrolouren97@gmail.com>
2025-04-13 00:27:49 -0300

    Unit Test

--- Project/src/calculators/calculator_1_test.py
+++ Project/src/calculators/calculator_1_test.py
@@ -1,8 +1,9 @@
 from typing import Dict
+from pytest import raises
 from .calculator_1 import Calculator1
 
 class MockRequest:
-    def __init__(self, body: Dict) -> None:
+    def __init__( self, body: Dict ) -> None:
         self.json = body
 
 
@@ -11,17 +12,26 @@ def test_calculate():
     calculator = 1
     result = 60.62
 
-    mock_request = MockRequest(body={ "number": number })
+    mock_request = MockRequest( body={ "number": number } )
     calculator_1 = Calculator1()
 
-    response = calculator_1.calculate(mock_request)
+    response = calculator_1.calculate( mock_request )
     
     assert "data" in response
-    assert "Calculator" in response["data"]
-    assert "result" in response["data"]
+    assert "Calculator" in response[ "data" ]
+    assert "result" in response[ "data" ]
 
-    assert response["data"]["result"] == result
-    assert response["data"]["Calculator"] == calculator
+    assert response[ "data" ][ "result" ] == result
+    assert response[ "data" ][ "Calculator" ] == calculator
 
     print()
     print(response)
+
+def test_calculate_with_body_error():
+    mock_request = MockRequest( body={ "test": 1 } )
+    calculator_1 = Calculator1()
+
+    with raises(Exception) as excinfo:
+        response = calculator_1.calculate( mock_request )
+
+    assert str(excinfo.value) == "Invalid body"
